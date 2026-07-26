6ba7c6225202b72698e245d5a5bbd494ebe02bb8
Eric Felipe Santos <ericfelipesantos2004@gmail.com>
2026-07-26 11:53:02 -0300

    Configuracao concluida da API do catalogo

--- Sophie_baby_kids/Sophie_baby_kids/settings.py
+++ Sophie_baby_kids/Sophie_baby_kids/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
+    'corsheaders',
     'catalogo',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,6 +135,10 @@ REST_FRAMEWORK = {
         "rest_framework.filters.OrderingFilter",
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
