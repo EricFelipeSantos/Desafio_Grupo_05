dc69bb01ab33a1b7fa45e80b78e8651e5df72ab2
Maria <mariasaldanha124@gmail.com>
2026-07-29 00:37:10 -0300

    fix: Configura banco para usar DATABASE_URL

--- Sophie_baby_kids/Sophie_baby_kids/settings.py
+++ Sophie_baby_kids/Sophie_baby_kids/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 from datetime import timedelta # função que permite definir os intervalos de tempo
 
@@ -86,14 +87,11 @@ WSGI_APPLICATION = 'Sophie_baby_kids.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sophie_baby_kids',
-        'USER': 'django_user',
-        'PASSWORD': 'BabyKids',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
