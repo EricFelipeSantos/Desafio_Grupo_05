00c8012ce91927f6084ed4f3e46c985794693ace
Maria <mariasaldanha124@gmail.com>
2026-07-28 21:02:59 -0300

    feat: ajustei .py para autenticação JWT no login

--- Sophie_baby_kids/usuarios/serializers.py
+++ Sophie_baby_kids/usuarios/serializers.py
@@ -1,27 +1,32 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-
 class UsuarioSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
-        fields = [
-            "id",
-            "username",
-            "email",
-            "password"
-        ]
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
-        extra_kwargs = {
-            "password": {
-                "write_only": True
-            }
-        }
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
 
-    def create(self, validated_data):
-        usuario = User.objects.create_user(
-            **validated_data
-        )
-
-        return usuario
+    def validate(self, data): # aqui ele está buscando o usuário pelo email e senha
+        email = data.get('email')
+        password = data.get('password')
+        
+        if not email or not password:
+            raise serializers.ValidationError('Email e senha são obrigatórios.')
+        
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError('Email não cadastrado.')
+        
+        if not user.check_password(password):
+            raise serializers.ValidationError('Senha incorreta.')
+        
+        if not user.is_active:
+            raise serializers.ValidationError('Usuário inativo.')
+        
+        data['user'] = user
+        return data
--- Sophie_baby_kids/usuarios/urls.py
+++ Sophie_baby_kids/usuarios/urls.py
@@ -1,12 +1,14 @@
 from rest_framework.routers import DefaultRouter
-from .views import UsuarioViewSet
-
+from django.urls import path
+from .views import UsuarioViewSet, LoginView
 
 router = DefaultRouter()
+router.register("usuarios", UsuarioViewSet)
 
-router.register(
-    "usuarios",
-    UsuarioViewSet
-)
+urlpatterns = router.urls + [ # rota para login
+    path('login/', LoginView.as_view(), name='login'),
+]
 
-urlpatterns = router.urls
+# eu criei um super usuário. Caso a cliente esqueça a senha e precise mudar, aí recomendo mudar
+# pelo terminal: 
+# python manage.py changepassword admin
