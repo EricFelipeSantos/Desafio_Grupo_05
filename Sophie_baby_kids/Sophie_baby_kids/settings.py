97121ca1153eb4983e2ffc1d5d4f83c91c1371ea
Eric Felipe Santos <ericfelipesantos2004@gmail.com>
2026-07-31 19:40:50 -0300

    Configura Django para deploy no Render

--- Sophie_baby_kids/Sophie_baby_kids/settings.py
+++ Sophie_baby_kids/Sophie_baby_kids/settings.py
@@ -22,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ej&77-97h=$)fi4t(c!5kb6wqq&+%^r+5d05n*i62g==c7ai2e'
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure-ej&77-97h=$)fi4t(c!5kb6wqq&+%^r+5d05n*i62g==c7ai2e"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -55,6 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,10 +91,10 @@ WSGI_APPLICATION = 'Sophie_baby_kids.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3', # trocar para: os.getenv('DATABASE_URL') na hora de fazer o deploy
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
-        ssl_require=False # muda para true dps
+        ssl_require=not DEBUG,
     )
 }
 
@@ -131,6 +135,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
