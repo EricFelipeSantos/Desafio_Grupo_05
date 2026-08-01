03b9f60ea164c65ea811247725e08a4a631c41f6
Maria <mariasaldanha124@gmail.com>
2026-08-01 00:50:35 -0300

    fix: apagando um espaço

--- Sophie_baby_kids/Sophie_baby_kids/settings.py
+++ Sophie_baby_kids/Sophie_baby_kids/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
     'sophie-baby-kids-api.onrender.com',
-    ' desafio-grupo-05.vercel.app',
+    'desafio-grupo-05.vercel.app',
 ]
 
 # Application definition
