53186313ac6f77c81e6d552efde4e711b4ea4900
Maria <mariasaldanha124@gmail.com>
2026-08-02 03:41:26 -0300

    fix: corrigindo o círculo ao selecionar 'colorido'

--- Sophie_baby_kids/frontend/src/pages/EditarProduto/EditarProduto.jsx
+++ Sophie_baby_kids/frontend/src/pages/EditarProduto/EditarProduto.jsx
@@ -623,8 +623,8 @@ function EditarProduto() {
                                     {cores.map((cor) => (
                                         <div className="selected-color" key={cor.nome}>
                                             <span
-                                                className="color-circle"
-                                                style={{ backgroundColor: cor.codigo }}
+                                                className={`color-circle ${cor.nome === "Colorido" ? "colorido" : ""}`}
+                                                style={cor.nome === "Colorido" ? {} : { backgroundColor: cor.codigo }}
                                             />
                                             <span>{cor.nome}</span>
                                             <button type="button" onClick={() => removerCor(cor.nome)}>
